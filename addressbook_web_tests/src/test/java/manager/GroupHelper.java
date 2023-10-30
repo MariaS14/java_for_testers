c1a5e803bcada9c2ee3e97538e3274e0e7d39f36
Maria <mariasosulnikova@gmail.com>
2023-10-30 21:50:24 +0300

    Задание №6: Реализовать двухуровневую архитектуру тестового набора

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -3,11 +3,11 @@ package manager;
 import model.GroupData;
 import org.openqa.selenium.By;
 
-public class GroupHelper {
-    private final ApplicationManager manager;
+public class GroupHelper extends HelperBase{
 
     public GroupHelper(ApplicationManager manager){
-        this.manager = manager;
+
+        super(manager);
     }
 
     public void openGroupsPage() {
@@ -90,13 +90,5 @@ public class GroupHelper {
         click(By.name("selected[]"));
 
 
-    }
-    private void click(By locator) {
-        manager.driver.findElement(locator).click();
-    }
-    private void type(By locator, String text) {
-        click(locator);
-        manager.driver.findElement(locator).clear();
-        manager.driver.findElement(locator).sendKeys(text);
     }
 }
