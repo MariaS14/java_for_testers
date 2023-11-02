5047fb2699472d0c49c2803fb98935afdcaa9be7
Maria <mariasosulnikova@gmail.com>
2023-11-02 13:07:55 +0300

    Поправила тест на удаление групп

--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -6,15 +6,12 @@ import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.interactions.Actions;
 import tests.TestBase;
-
 
 
 import java.io.File;
@@ -24,47 +21,65 @@ import static org.junit.Assert.assertThat;
 import static org.openqa.selenium.chrome.ChromeDriverService.createDefaultService;
 
 public class ContactRemovalTests extends TestBase {
-  private static WebDriver driver;
+    private static WebDriver driver;
 
-  @BeforeEach
-  public void setUp() {
+    @BeforeEach
+    public void setUp() {
 
-    if (driver == null) {
-      var service = createDefaultService();
-      service.setExecutable("c:/windows/system32/chromedriver.exe");
-      driver = new ChromeDriver(service);
+        if (driver == null) {
+            var service = createDefaultService();
+            service.setExecutable("c:/windows/system32/chromedriver.exe");
+            driver = new ChromeDriver(service);
+        }
+        driver.get("http://localhost/addressbook/");
+        driver.manage().window().setSize(new Dimension(1184, 784));
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+
     }
-    driver.get("http://localhost/addressbook/");
-    driver.manage().window().setSize(new Dimension(1184, 784));
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
 
-  }
+    @AfterEach
+    public void tearDown() {
+        driver.findElement(By.linkText("Logout")).click();
+        driver.quit();
+    }
 
-  @AfterEach
-  public void tearDown() {
-    driver.findElement(By.linkText("Logout")).click();
-    driver.quit();
-  }
+    @Test
 
-  @Test
+    public void canRemoveContact() {
+        driver.findElement(By.linkText("home")).click();
+        if (!isElementPresent(By.cssSelector("#maintable input[type='checkbox']:first-child"))) {
+            driver.findElement(By.linkText("add new")).click();
+            {
+                WebElement element = driver.findElement(By.name("firstname"));
+                Actions builder = new Actions(driver);
+                builder.doubleClick(element).perform();
+            }
+            driver.findElement(By.name("firstname")).click();
+            driver.findElement(By.name("firstname")).sendKeys("contact name");
+            driver.findElement(By.name("lastname")).click();
+            driver.findElement(By.name("lastname")).sendKeys("contact name");
+            driver.findElement(By.name("middlename")).click();
+            driver.findElement(By.name("middlename")).sendKeys("contact name");
+            driver.findElement(By.name("home")).sendKeys("contact phone");
+            driver.findElement(By.cssSelector("input:nth-child(87)")).click();
+            driver.findElement(By.linkText("home page")).click();
+        }
 
-  public void canRemoveContact() {
+        driver.findElement(By.cssSelector("#maintable input[type='checkbox']:first-child")).click();
+        driver.findElement(By.cssSelector("input[value='Delete']")).click();
+        MatcherAssert.assertThat(driver.switchTo().alert().getText(), is("Delete 1 addresses?"));
+        driver.switchTo().alert().accept();
 
-    driver.findElement(By.cssSelector("#maintable input[type='checkbox']:first-child")).click();
-    driver.findElement(By.cssSelector("input[value='Delete']")).click();
-    MatcherAssert.assertThat(driver.switchTo().alert().getText(), is("Delete 1 addresses?"));
-    driver.switchTo().alert().accept();
-
-  }
+    }
 
 
     //driver.findElement(By.id("1")).click();
     //driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
     //MatcherAssert.assertThat(driver.switchTo().alert().getText(), is("Delete 1 addresses?"));
     //driver.switchTo().alert().accept();
-  //}
+    //}
 
 
 
@@ -80,14 +95,14 @@ public class ContactRemovalTests extends TestBase {
   }*/
 
 
-  protected boolean isElementPresent(By locator) {
+    protected boolean isElementPresent(By locator) {
 
-    try {
-      driver.findElement(locator);
-      return true;
-    } catch (NoSuchElementException exception) {
-      return false;
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
     }
-  }
 }
 
