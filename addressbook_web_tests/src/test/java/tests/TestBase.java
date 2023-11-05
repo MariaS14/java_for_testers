61724d3cfd5f1ce1eb2fd7bf659ec2b881d9a7f9
Maria <mariasosulnikova@gmail.com>
2023-11-05 19:57:19 +0300

    Параметризированные тесты

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,6 +3,8 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.Random;
+
 import static org.hamcrest.CoreMatchers.is;
 
 public class TestBase {
@@ -17,4 +19,15 @@ public class TestBase {
             app.init(System.getProperty("browser", "firefox"));
         }
     }
+
+    public static String randomString(int n) {
+        var rnd = new Random();
+        var result = "";
+        for (int i = 0; i < n; i++) {
+            result = result + (char) ('a' + rnd.nextInt(26));
+        }
+
+        return result;
+    }
 }
+
