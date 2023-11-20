3ea81044a40a728e446ccf3ef57668b064e65383
Maria <mariasosulnikova@gmail.com>
2023-11-20 09:01:40 +0300

    Лекция 8.6. Извлечение ссылки из письма, удаление писем из почтового ящика

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
@@ -16,13 +16,9 @@ public class MailHelper extends HelperBase{
 
     public List<MailMessage> receive(String username, String password, Duration duration) {
         var start = System.currentTimeMillis();
-        while (System.currentTimeMillis() < start + duration.toMillis())
-        {
+        while (System.currentTimeMillis() < start + duration.toMillis()) {
             try {
-                var session = Session.getInstance(new Properties());
-                Store store = session.getStore("pop3");
-                store.connect("localhost",username, password);
-                var inbox = store.getFolder("INBOX");
+                var inbox = getInbox(username, password);
                 inbox.open(Folder.READ_ONLY);
                 var messages = inbox.getMessages();
                 var result = Arrays.stream(messages)
@@ -37,16 +33,51 @@ public class MailHelper extends HelperBase{
                         })
                         .toList();
                 inbox.close();
-                store.close();
+                inbox.getStore().close();
                 if (result.size() > 0) {
                     return result;
                 }
             } catch (MessagingException e) {
                 throw new RuntimeException(e);
             }
-
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         throw new RuntimeException("No mail");
     }
-    Thread
+
+    private static Folder getInbox(String username, String password) {
+        try {
+            var session = Session.getInstance(new Properties());
+            Store store = session.getStore("pop3");
+            store.connect("localhost", username, password);
+            var inbox = store.getFolder("INBOX");
+            return inbox;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void drain(String username, String password) { //очищение почтового ящика
+        var inbox = getInbox(username, password);
+        try {
+            inbox.open(Folder.READ_WRITE);
+            Arrays.stream(inbox.getMessages()).forEach(m -> {
+                try {
+                    m.setFlag(Flags.Flag.DELETED, true);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            inbox.close();
+            inbox.getStore().close();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
