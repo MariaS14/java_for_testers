db519d93e442ab87ae947f8d7fe97b6e84fd0f12
Maria <mariasosulnikova@gmail.com>
2023-11-24 11:47:10 +0300

    ДЗ 20

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -90,7 +90,7 @@ public class GroupHelper extends HelperBase {
     }
 
     private void selectGroup(GroupData group) {
-        click(By.cssSelector(String.format("input2[value='%s']", group.id())));
+        click(By.cssSelector(String.format("input[value='%s']", group.id())));
 
     }
 
